3ab2d25936e96030fc530cf04b9ec1c6d6e58f1a
IT Shop <IT Shop@DESKTOP-BIH0M27>
2021-11-27 00:46:36 +0200

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> Yossef = new LinkedList<string>();
+
+            Yossef.AddFirst("ALOOO");
+            Yossef.AddFirst("ALOO");
+            Yossef.AddLast("ALOOOO");
+            Yossef.AddLast("Done");
+
+            Console.WriteLine("The linked List Before Removing:  ");
+
+            foreach (var item in Yossef)
+            {
+                Console.WriteLine(item);
+            }
+            Yossef.Remove("ALOOO");
+            Yossef.RemoveLast();
+            Yossef.RemoveFirst();
+            Console.WriteLine();
+            Console.WriteLine("The linked List After Removing: ");
+
+            foreach (var item in Yossef)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
